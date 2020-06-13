d44e13e7c3dd26207fb1677a94cae6eb12a3e2e3
ep3824 <ethan.parent1@gmail.com>
2020-06-13 16:17:47 -0600

    Add cow pics

--- src/components/app/Cow.jsx
+++ src/components/app/Cow.jsx
@@ -16,8 +16,9 @@ class Cow extends React.Component {
     return (
       <div>
           <div className="showcase">{this.state.showcasedCow}</div>
-          <div className={`${this.props.className}`} onClick={this.props.handleClick}>{this.props.name}</div>
+          <div className={`${this.props.className}`} onClick={this.props.handleClick}>{this.props.name}<img alt="cow" src="https://s3.amazonaws.com/kandipatternspatterns/animals/4331-Cow.png"></img></div>
           <p style={ this.props.className !== 'cow0' ? {display:'none'} : {display: 'block'} } className={`${this.props.className}description`}>{this.props.description}</p>
+          
       </div>
     )
   }
